c20dab1e32430ce68dbed07ae77d9fea8f2d203b
Artem Shestakov <arartemtem@gmail.com>
2017-03-23 17:23:13 +0300

    4 done

---
+++ seminar2/only_even_int.py
@@ -0,0 +1,28 @@
+class only_even_int():
+    '''This class at to list only even numbers of type int'''
+
+    message = 'I LIKE ONLY INT TYPE AND I DON\'T LIKE ODD NUMBERS!'
+
+    def __init__(self, values=None):
+        if values is None:
+            self.values = []
+        else:
+            self.values = values
+            for i in range(len(values)):
+                if type(values[i]) == int and values[i] % 2 == 0:
+                    pass
+                else:
+                    values[i] = only_even_ints.message
+
+    def __getitem__(self, key):
+        return self.values[key]
+
+    def __setitem__(self, key, value):
+        if type(value) == int and value % 2 == 0:
+            self.values[key] = value
+        else:
+            self.values[key] = only_even_ints.message
+
+values_list = [2, 4, 6, 8, 10, 1, 3, 5, 7, 9, 'one', 'two', 5.5, 3.3]
+my_list = only_even_ints(values_list)
+print(my_list[:])
